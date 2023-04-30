9714b2919a70c394ff01b2dd9239dd14dfd376db
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-04-30 16:02:04 -0700

    added another cursed query
    heheheha

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ Also, feel free to add some more 😏 queries to the list.
 
 function genFunnyQuery() {
     var linkStart = "https://www.google.com/search?q=";
-    var sillyQueries = ["endangered%20animal%20recipes", "pron", "child%20pron", "cocomelon%20r34", "what%20does%20coom%20taste%like", "pokemon%20inflation", "cocaine%20purchase" "h3ntai%20pics%20free", "free%20drugs", "how%20to%20get%20b1tches"];
+    var sillyQueries = ["endangered%20animal%20recipes", "pron", "child%20pron", "cocomelon%20r34", "what%20does%20coom%20taste%like", "pokemon%20inflation", "cocaine%20purchase" "h3ntai%20pics%20free", "free%20drugs", "how%20to%20get%20b1tches", "men%20kissing"];
     var randQuery = sillyQueries[Math.floor(Math.random()*sillyQueries.length)];
     var fullQuery = linkStart + randQuery;
     window.open(fullQuery);
